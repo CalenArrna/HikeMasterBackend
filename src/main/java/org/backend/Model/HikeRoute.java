0316173bcca0e7f55189fc2f4663f8190a672cf3
Elod93 <elod.kristaly@gmail.com>
2020-07-14 14:15:33 +0200

    postgresql is ready

--- src/main/java/org/backend/Model/HikeRoute.java
+++ src/main/java/org/backend/Model/HikeRoute.java
@@ -1,20 +1,13 @@
 package org.backend.Model;
-
-import jdk.javadoc.internal.doclets.toolkit.util.Utils;
-
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class HikeRoute {
     @Id
     @GeneratedValue
     private long routeId;
-    @OneToMany
-    private  List<Messages>messages=new ArrayList<>();
-    @Column
-    private int rate;
+
+
     @Column
     private double coordinates;
 }
