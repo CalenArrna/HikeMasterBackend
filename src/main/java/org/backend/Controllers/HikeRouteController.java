444e06f656e8493cb48ef300345598bdbbf8257c
Elod93 <elod.kristaly@gmail.com>
2020-07-24 16:53:20 +0200

    heroku last commit in friday

--- src/main/java/org/backend/Controllers/HikeRouteController.java
+++ src/main/java/org/backend/Controllers/HikeRouteController.java
@@ -1,6 +1,5 @@
 package org.backend.Controllers;
 
-import org.backend.Repository.HikeRouteRepository;
 import org.backend.DTOs.HikeRouteErrorDTO;
 import org.backend.DTOs.HikeRouteSuccessDTO;
 import org.backend.DTOs.ResponseDTO;
@@ -9,7 +8,6 @@ import org.backend.Model.Message;
 import org.backend.Repository.HikeRouteRepository;
 import org.backend.Repository.MessageRepository;
 import org.backend.Service.HikeRouteService;
-import org.backend.Service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,20 +23,19 @@ public class HikeRouteController {
     MessageRepository messageRepository;
 
     @Autowired
-    public HikeRouteController(HikeRouteService hikeRouteService, HikeRouteService messageService,HikeRouteRepository hikeRouteRepository, MessageRepository messageRepository) {
+    public HikeRouteController(HikeRouteService hikeRouteService, HikeRouteService messageService, HikeRouteRepository hikeRouteRepository, MessageRepository messageRepository) {
         this.hikeRouteService = hikeRouteService;
         this.messageService = messageService;
-        this.hikeRouteRepository=hikeRouteRepository;
+        this.hikeRouteRepository = hikeRouteRepository;
         this.messageRepository = messageRepository;
     }
 
     @GetMapping(value = "/hike_route/{route_Id}")
     public ResponseDTO getHikeRouteDetails(@PathVariable Long route_Id) {
         HikeRoute hikeRoute = hikeRouteService.hikeRouteDetails(route_Id);
-        if (hikeRoute == null){
+        if (hikeRoute == null) {
             return new HikeRouteErrorDTO();
-        }
-        else{
+        } else {
             HikeRouteSuccessDTO hikeRouteSuccessDTO = new HikeRouteSuccessDTO();
             hikeRouteSuccessDTO.setHikeRoute(hikeRoute);
             return hikeRouteSuccessDTO;
@@ -46,7 +43,13 @@ public class HikeRouteController {
     }
 
     @PostMapping(value = "/hike_route")
-    public ResponseDTO postHikeRoute(@RequestBody String tour_type, String route_type, String difficultly, Integer tour_length, Integer level_rise,Integer rate) {
+
+    public ResponseDTO postHikeRoute(@RequestParam(required = false) String tour_type,
+                                     @RequestParam(required = false) String route_type,
+                                     @RequestParam(required = false) String difficultly,
+                                     @RequestParam(required = false) Integer tour_length,
+                                     @RequestParam(required = false) Integer level_rise,
+                                     @RequestParam(required = false) Integer rate) {
         List<HikeRoute> routesByParams = hikeRouteService.findHikeRoutesByParams(tour_type, route_type, difficultly, tour_length, level_rise, rate);
         if (routesByParams.isEmpty()) {
             return new HikeRouteErrorDTO();
@@ -58,12 +61,11 @@ public class HikeRouteController {
     }
 
     @RequestMapping(value = "/hike_route/{route_Id}/messages", method = RequestMethod.POST)
-    public String addMessageToRoute(@PathVariable Long route_Id, @RequestBody Message message ) {
-        if(hikeRouteRepository.findById(route_Id).isPresent()){
+    public String addMessageToRoute(@PathVariable Long route_Id, @RequestBody Message message) {
+        if (hikeRouteRepository.findById(route_Id).isPresent()) {
             hikeRouteRepository.findById(route_Id).get().getMessages().add(messageRepository.save(message));
             return "success";
-        }
-        else {
+        } else {
             return "failed";
         }
     }
